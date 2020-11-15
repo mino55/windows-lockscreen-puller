c5e210b3d3c1b6c9351cedb19a956a9bdc76ed79
mino55 <mikael_55555@hotmail.com>
2020-11-15 13:11:15 +0100

    always use script dir for collected + read config from txt

--- collect.py
+++ collect.py
@@ -5,11 +5,21 @@ from PIL import Image
 from datetime import date
 import shutil
 import importlib
-import collect_config
 from os.path import expanduser
+import pathlib
+from pathlib import PurePosixPath
 
 HOME = expanduser("~")
-ASSET_PATH = HOME + collect_config.collect_from
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+COLLECT_CONFIG_PATH = os.path.join(SCRIPT_DIR, 'config.txt')
+
+def read_config():
+  with open(COLLECT_CONFIG_PATH, 'r') as file:
+    data = file.read().replace('\n', '')
+    return data
+
+
+ASSET_PATH = HOME + read_config()
 
 def start_at(asset_path):
     for root, dirs, files in os.walk(asset_path):
@@ -21,7 +31,7 @@ def sift_files(files, in_dir):
     for file in files:
         path_to_image = os.path.join(in_dir, file)
         image = open_image(path_to_image)
-        if image != None: sift_image(image, path_to_image, os.getcwd())
+        if image != None: sift_image(image, path_to_image, SCRIPT_DIR)
         else: print(str(file) + ' is not an image file.')
 
 def open_image(path_to_image):
@@ -36,19 +46,20 @@ def sift_image(image, copy_from_path, copy_to_dir):
         name = str(date.today()) + '-' + str(uuid.uuid4()) + '.jpg'
         copy_to_path = os.path.join(copy_to_dir, name)
         shutil.copy(copy_from_path, copy_to_path)
-        print('From: ' + copy_from_path)
         print('Created: ' + name + ' (' + str(width) + 'x' + str(height) + ')')
+        print('From: ' + copy_from_path)
+        print('To: ' + copy_to_path)
+        print('--- --- --- --- ---')
 
 def get_config_path():
     try:
-        print('Collect from:' + ASSET_PATH)
+        print('Collect from: ' + ASSET_PATH)
+        print('Collect to: ' +  SCRIPT_DIR)
         input('Is this ok? If so, press any key to continue')
+        print('--- --- --- --- ---')
         return ASSET_PATH
     except:
         print('WARNING: Collect_from inside of collect_config.py not found')
-        print('Collect from: ' + os.getcwd())
-        input('Is this ok? If so, press any key to continue')
-        return os.getcwd()
 
 
 start_at(get_config_path())
