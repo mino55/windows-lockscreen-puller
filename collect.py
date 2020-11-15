6a10dfe3ffb1418f4bbe070dda228ed9b6cb6d53
mino55 <mikael_55555@hotmail.com>
2020-11-15 10:56:18 +0100

    adjust project for Python3

--- collect.py
+++ collect.py
@@ -19,8 +19,8 @@ def sift_image(image, copy_from_file, copy_to_dir):
         name = str(date.today()) + '-' + str(uuid.uuid4()) + '.jpg'
         copy_to_file = os.path.join(copy_to_dir, name)
         shutil.copy(copy_from_file, copy_to_file)
-        print 'From: ' + copy_from_file
-        print 'Created: ' + name + ' (' + str(width) + 'x' + str(height) + ')'
+        print('From: ' + copy_from_file)
+        print('Created: ' + name + ' (' + str(width) + 'x' + str(height) + ')')
 
 def sift_files(files, in_dir):
     for file in files:
@@ -38,14 +38,14 @@ def start_at(asset_path):
 def get_config_path():
     try:
         print('Collect from:' + collect_config.collect_from)
-        raw_input('Is this ok? If so, press any key to continue')
+        input('Is this ok? If so, press any key to continue')
         return collect_config.collect_from
     except:
-        print 'WARNING: Collect_from inside of collect_config.py not found'
-        print 'Collect from: ' + os.getcwd()
-        raw_input('Is this ok? If so, press any key to continue')
+        print('WARNING: Collect_from inside of collect_config.py not found')
+        print('Collect from: ' + os.getcwd())
+        input('Is this ok? If so, press any key to continue')
         return os.getcwd()
 
 # get_config_path()
 start_at(get_config_path())
-raw_input('Done! Press any key to quit')
+input('Done! Press any key to quit')
