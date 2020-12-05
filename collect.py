c1cb6611bddc8824430316309a09f4e99c09bf96
mino55 <mikael_55555@hotmail.com>
2020-12-05 21:09:00 +0100

    validate images and separate image formats to different dirs

--- collect.py
+++ collect.py
@@ -14,28 +14,41 @@ HOME = expanduser("~")
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 COLLECT_CONFIG_PATH = os.path.join(SCRIPT_DIR, 'config.json')
 CONFIG = {
-  "assetsPath": None,
-  "desktopPath": None,
-  "mobilePath": None,
-  "minWidth": None,
-  "minHeight": None,
-  "autoRemoveDuplicates": None,
-  "saveDiscarded": None,
-  "discardPath": None
+    "assetsPath": None,
+    "desktopPath": None,
+    "mobilePath": None,
+    "minWidth": None,
+    "minHeight": None,
+    "autoRemoveDuplicates": None,
+    "saveDiscarded": None,
+    "discardPath": None
 }
 
+class ImageFormat:
+    DESKTOP = "desktop"
+    MOBILE = "mobile"
+
 def read_config():
-  with open(COLLECT_CONFIG_PATH, 'r') as file:
-    json_str = file.read().replace('\n', '')
-    loaded_config = json.loads(json_str)
-    CONFIG["assetsPath"] = HOME + loaded_config["assetsPath"]
-    CONFIG["desktopPath"] = loaded_config["desktopPath"]
-    CONFIG["mobilePath"] = loaded_config["mobilePath"]
-    CONFIG["minWidth"] = loaded_config["minWidth"]
-    CONFIG["minHeight"] = loaded_config["minHeight"]
-    CONFIG["autoRemoveDuplicates"] = loaded_config["autoRemoveDuplicates"]
-    CONFIG["saveDiscarded"] = loaded_config["saveDiscarded"]
-    CONFIG["discardPath"] = loaded_config["discardPath"]
+    with open(COLLECT_CONFIG_PATH, 'r') as file:
+        json_str = file.read().replace('\n', '')
+        loaded_config = json.loads(json_str)
+        CONFIG["assetsPath"] =  HOME + loaded_config["assetsPath"]
+        CONFIG["desktopPath"] = loaded_config["desktopPath"]
+        CONFIG["mobilePath"] = loaded_config["mobilePath"]
+        CONFIG["minWidth"] = loaded_config["minWidth"]
+        CONFIG["minHeight"] = loaded_config["minHeight"]
+        CONFIG["discardSquares"] = loaded_config["discardSquares"]
+        CONFIG["autoRemoveDuplicates"] = loaded_config["autoRemoveDuplicates"]
+        CONFIG["saveDiscarded"] = loaded_config["saveDiscarded"]
+        CONFIG["discardPath"] = loaded_config["discardPath"]
+
+def setup_dirs():
+    desktop_path = CONFIG["desktopPath"]
+    mobile_path = CONFIG["mobilePath"]
+    if not os.path.exists(desktop_path):
+        os.mkdir(desktop_path)
+    if not os.path.exists(mobile_path):
+        os.mkdir(mobile_path)
 
 def start_at(asset_path):
     for root, dirs, files in os.walk(asset_path):
@@ -45,10 +58,13 @@ def start_at(asset_path):
 
 def sift_files(files, in_dir):
     for file in files:
-        path_to_image = os.path.join(in_dir, file)
-        image = open_image(path_to_image)
-        if image != None: sift_image(image, path_to_image, SCRIPT_DIR)
-        else: print(str(file) + ' is not an image file.')
+        path_to_file = os.path.join(in_dir, file)
+        image = open_image(path_to_file)
+        if image == None:
+            print(str(file) + ' is not an image file.')
+            return
+        print("For file: " + str(file));
+        sift_image(image, path_to_file)
 
 def open_image(path_to_image):
     try:
@@ -56,16 +72,51 @@ def open_image(path_to_image):
     except:
         return None
 
-def sift_image(image, copy_from_path, copy_to_dir):
-    width, height = image.size
-    if width > CONFIG["minWidth"] and height > CONFIG["minHeight"]:
-        name = str(date.today()) + '-' + str(uuid.uuid4()) + '.jpg'
-        copy_to_path = os.path.join(copy_to_dir, name)
-        shutil.copy(copy_from_path, copy_to_path)
-        print('Created: ' + name + ' (' + str(width) + 'x' + str(height) + ')')
-        print('From: ' + copy_from_path)
-        print('To: ' + copy_to_path)
+def sift_image(image, copy_from_path):
+    if not image_is_valid(image):
+        print("Fails validation")
         print('--- --- --- --- ---')
+        return
+    img_format = image_format(image)
+    if img_format == ImageFormat.DESKTOP:
+        save_image(image, copy_from_path, CONFIG["desktopPath"])
+    if img_format == ImageFormat.MOBILE:
+        save_image(image, copy_from_path, CONFIG["mobilePath"])
+
+
+def image_is_valid(image):
+    width, height = image.size
+    if CONFIG["discardSquares"] and width == height:
+        print("Square image: " + str(width) + ", " + str(height))
+        return False
+    if CONFIG["minWidth"] and width < CONFIG["minWidth"]:
+        print("Too small width: " + str(width))
+        return False
+    if CONFIG["minHeight"] and height < CONFIG["minHeight"]:
+        print("Too small height: " + str(height))
+        return False
+    return True
+
+def image_format(image):
+    width, height = image.size
+    if width > height:
+        return ImageFormat.DESKTOP
+    if height > width:
+        return ImageFormat.MOBILE
+    else:
+        exc_msg = "Image with errous width, height: " + str(width) + ", " + str(height)
+        raise Exception(exc_msg)
+
+
+def save_image(image, copy_from_path, copy_to_dir):
+    width, height = image.size
+    name = str(date.today()) + '-' + str(uuid.uuid4()) + '.jpg'
+    copy_to_path = os.path.join(copy_to_dir, name)
+    shutil.copy(copy_from_path, copy_to_path)
+    print('Created: ' + name + ' (' + str(width) + 'x' + str(height) + ')')
+    print('From: ' + copy_from_path)
+    print('To: ' + copy_to_path)
+    print('--- --- --- --- ---')
 
 def get_config_path():
     try:
@@ -79,5 +130,6 @@ def get_config_path():
 
 
 read_config()
+setup_dirs()
 start_at(get_config_path())
 input('Done! Press any key to quit')
